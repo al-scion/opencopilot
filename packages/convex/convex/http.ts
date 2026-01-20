630cd4e77dd91f9165623f6c130d9ce3d3462e72
al-scion <abel@withscion.com>
2026-01-20 18:29:18 +0800

    fixed convex router

--- packages/convex/convex/http.ts
+++ packages/convex/convex/http.ts
@@ -1,20 +1,12 @@
-import { httpRouter } from "convex/server";
-import { corsRouter } from "convex-helpers/server/cors";
-import { httpAction } from "./_generated/server";
+import { type HonoWithConvex, HttpRouterWithHono } from "convex-helpers/server/hono";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { type ActionCtx, httpAction } from "./_generated/server";
 
-const http = httpRouter();
-const app = corsRouter(http, {
-	allowedOrigins: ["*"],
-	allowedHeaders: ["*"],
-	// debug: true,
+const app = new Hono<{ Bindings: ActionCtx }>();
+app.use(cors());
+app.get("/", async (c) => {
+	c.text("Hello from Convex with Hono!");
 });
 
-app.route({
-	path: "/health",
-	method: "GET",
-	handler: httpAction(async (ctx, req) => {
-		return new Response("ok");
-	}),
-});
-
-export default app.http;
+export default new HttpRouterWithHono(app);
