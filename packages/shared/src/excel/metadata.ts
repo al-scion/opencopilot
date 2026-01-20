aa319a62a76dcb3929627177758da3738f2ab032
al-scion <abel@withscion.com>
2026-01-20 16:50:14 +0800

    fixed-metadata-key

--- packages/shared/src/excel/metadata.ts
+++ packages/shared/src/excel/metadata.ts
@@ -4,7 +4,9 @@ export const officeMetadataSchema = z.object({
 	id: z.string(),
 });
 
+export const METADATA_STORAGE_KEY = "office-metadata";
+
 export const getOfficeMetadata = (): z.infer<typeof officeMetadataSchema> => {
-	const metadata = Office.context.document.settings.get("office-metadata");
+	const metadata = Office.context.document.settings.get(METADATA_STORAGE_KEY);
 	return officeMetadataSchema.parse(metadata);
 };
