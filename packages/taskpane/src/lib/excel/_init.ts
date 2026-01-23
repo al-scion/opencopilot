8c41103d83555be007d5aea67ee06d2f1d3a8b6d
al-scion <abel@withscion.com>
2026-01-24 00:24:40 +0800

    fixes

--- packages/taskpane/src/lib/excel/_init.ts
+++ packages/taskpane/src/lib/excel/_init.ts
@@ -1,19 +1,24 @@
 import { registerNamedRange } from "@packages/shared";
-import { registerCustomFunctions, registerShortcuts } from "@/lib/excel/config";
+// import { registerCustomFunctions, registerShortcuts } from "@/lib/excel/config";
 import { useAppState } from "@/lib/state";
+import { generateImage, generateText, memoize } from "./formula";
+import { toggleTaskpane } from "./shortcuts";
 
 export const initWorkbook = async () => {
 	await Office.onReady(async ({ host, platform }) => {
 		// First, wait for the office to be ready and execute
 		Office.addin.showAsTaskpane().then(() => useAppState.setState({ taskpaneOpen: true }));
 		// Then implement shortcuts and custom functions using the Office API
-		registerShortcuts();
-		registerCustomFunctions();
+		// registerShortcuts();
+		// registerCustomFunctions();
+
+		CustomFunctions.associate({
+			"GENERATE.TEXT": memoize(generateText),
+			"GENERATE.IMAGE": memoize(generateImage),
+		});
+		Office.actions.associate("toggleTaskpane", toggleTaskpane);
+
 		// All of this will stay pending until the user exits cell edit state
 		await registerNamedRange();
-		// Office.addin.beforeDocumentCloseNotification.enable();
-		// Office.addin.beforeDocumentCloseNotification.onCloseActionCancelled(() =>
-		// 	console.log("beforeDocumentCloseNotification")
-		// );
 	});
 };
--- packages/taskpane/src/lib/excel/config.ts
+++ packages/taskpane/src/lib/excel/config.ts
@@ -1,6 +1,4 @@
 import { IMAGE_MODELS, LANGUAGE_MODELS } from "@packages/shared";
-import { generateImage, generateText, memoize } from "./formula";
-import { toggleTaskpane } from "./shortcuts";
 
 // References
 // WARNING: DO NOT EDIT THIS TYPE DEFINITION!!
@@ -98,12 +96,10 @@ const shortcutsDefinitions = [
 			windows: "Ctrl+J",
 			mac: "Command+J",
 		},
-		action: toggleTaskpane,
 	},
 ] as const satisfies {
 	id: string;
 	key: ShortcutsConfig["shortcuts"][number]["key"];
-	action: Function;
 }[];
 
 const customFunctionsDefinitions = [
@@ -114,7 +110,6 @@ const customFunctionsDefinitions = [
 			{ name: "prompt", type: "string" },
 			{ name: "model", optional: true, customEnumId: "LANGUAGE_MODELS", type: "string" },
 		],
-		action: generateText,
 	},
 	{
 		id: "GENERATE.IMAGE",
@@ -123,16 +118,14 @@ const customFunctionsDefinitions = [
 			{ name: "prompt", type: "string" },
 			{ name: "model", optional: true, customEnumId: "IMAGE_MODELS", type: "string" },
 		],
-		action: generateImage,
 	},
 ] as const satisfies {
 	id: string;
 	description?: string;
 	parameters: CustomFunctionsConfig["functions"][number]["parameters"];
-	action: Function;
 }[];
 
-export const customFunctionsConfig = {
+export const customFunctionsConfig: CustomFunctionsConfig = {
 	allowCustomDataForDataTypeAny: true,
 	allowErrorForDataTypeAny: true,
 	functions: customFunctionsDefinitions.map((item) => ({
@@ -174,16 +167,4 @@ export const shortcutsConfig: ShortcutsConfig = {
 		action: item.id,
 		key: item.key,
 	})),
-};
-
-export const registerCustomFunctions = () => {
-	CustomFunctions.associate(
-		Object.fromEntries(customFunctionsDefinitions.map((item) => [item.id, memoize(item.action)]))
-	);
-};
-
-export const registerShortcuts = () => {
-	shortcutsDefinitions.forEach((item) => {
-		Office.actions.associate(item.id, item.action);
-	});
 };
